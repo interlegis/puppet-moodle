e659add905e65cf66b8a205c69187bb8866e73b3
Fabio Rauber <fabiorauber@gmail.com>
2013-11-14 08:49:30 -0200

    Added some tuning

--- manifests/init.pp
+++ manifests/init.pp
@@ -47,8 +47,12 @@ class moodle ( 	$source = "https://github.com/moodle/moodle",
 
 	class { 'apache':
                 mpm_module => 'prefork',
+		keepalive => 'off',
+		keepalive_timeout => '4',
+		timeout => '45',
         }
         include apache::mod::php
+	include apache::mod::expires
         
 	# Vhost with serveradmin
         apache::vhost { $vhostname:
@@ -57,6 +61,19 @@ class moodle ( 	$source = "https://github.com/moodle/moodle",
                 serveradmin => $serveradmin,
                 access_log_file => 'access_moodle.log',
                 error_log_file => 'error_moodle.log',
+		options => ['Indexes','FollowSymLinks'],
+		custom_fragment => '<IfModule mod_expires.c>
+                        ExpiresActive On
+                        ExpiresDefault "access plus 1 seconds"
+                        ExpiresByType text/html "access plus 1 seconds"
+                        ExpiresByType image/gif "access plus 1 week"
+                        ExpiresByType image/jpeg "access plus 1 week"
+                        ExpiresByType image/png "access plus 1 week"
+                        ExpiresByType text/css "access plus 1 week"
+                        ExpiresByType text/javascript "access plus 1 week"
+                        ExpiresByType application/x-javascript "access plus 1 week"
+                        ExpiresByType text/xml "access plus 1 seconds"
+                </IfModule> ',
         }
 
 	# Install package requirements
@@ -74,5 +91,28 @@ class moodle ( 	$source = "https://github.com/moodle/moodle",
 		require => Git::Clone["moodle"],
 	}
 
+	# PHP Cache Config
+	php::module { [ 'apc' ]: }
+    	php::module::ini { 'apc':
+      		settings => {
+        		'apc.enabled'      => '1',
+        		'apc.shm_segments' => '1',
+        		'apc.shm_size'     => '128M',
+			'apc.stat'	   => '0',	
+      		}
+    	}
+
+	# Moodle Cron
+	$cron_i = 5*fqdn_rand(2)
+	cron { "admin cron.php":
+                command => "/usr/bin/php $installdir/moodle/admin/cron.php",
+                minute => [0+$cron_i,10+$cron_i,20+$cron_i,30+$cron_i,40+$cron_i,50+$cron_i],
+                ensure => present,
+                environment => [
+                        "MAILTO=$serveradmin",
+                        "SHELL=/bin/bash",
+                ],
+                require => Git::Clone["moodle"],
+        }
 
 }
